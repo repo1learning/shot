811d0b80beff526679fdcf019656a78625adbca8
ManU <ManU@Ashish>
2019-12-18 01:07:47 +0530

    new one

--- src/com/sshot/ShotToWordPad.java
+++ src/com/sshot/ShotToWordPad.java
@@ -10,46 +10,63 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
+
 import javax.imageio.ImageIO;
+
 import org.apache.poi.util.Units;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 
 public class ShotToWordPad {
+	static XWPFDocument doc = new XWPFDocument();
+	static XWPFParagraph p = doc.createParagraph();
+	static XWPFRun xwpfRun = p.createRun();
+	static FileOutputStream out = null;
+	static FileInputStream input = null;
+	static XWPFTable table = doc.createTable();
 
-	public static void saveImageToWord(String documentName,ArrayList<String> screens) throws Exception, FileNotFoundException, IOException {
-
-		XWPFDocument doc = new XWPFDocument();
-		XWPFParagraph p = doc.createParagraph();
-		XWPFRun xwpfRun = p.createRun();
+	public static void saveImageToWord(String documentName, ArrayList<String> screens)
+			throws Exception, FileNotFoundException, IOException {
 
 		for (String imgFile : screens) {
 			int format = XWPFDocument.PICTURE_TYPE_JPEG;
 			xwpfRun.addBreak();
-			xwpfRun.addPicture(new FileInputStream(imgFile), format, imgFile, Units.toEMU(400), Units.toEMU(200));
-		}
+			input = new FileInputStream(imgFile);
+			xwpfRun.addPicture(input, format, imgFile, Units.toEMU(400), Units.toEMU(200));
 
-		FileOutputStream out = new FileOutputStream(documentName);
+			input.close();
+		}
+		out = new FileOutputStream(documentName);
 		doc.write(out);
 		out.close();
+		doc.close();
 	}
 
+	public static void addText(String documentName, int row, String text) throws IOException {
+		out = new FileOutputStream(new File(documentName));
+		// create first row
+		table.setWidth(60);
+		XWPFTableRow tableRow = table.getRow(row);
+		
+		tableRow.getCell(0).setText(text);
+		doc.write(out);
+	}
 	
-	public static void takeScreeShot(String fileName) {
 
+	public static void takeScreeShot(String fileName) {
 		try {
 			Robot robot = new Robot();
 			String format = "jpg";
-			
 			Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
 			BufferedImage screenFullImage = robot.createScreenCapture(screenRect);
 			ImageIO.write(screenFullImage, format, new File(fileName));
-				} catch (AWTException | IOException ex) {
+		} catch (AWTException | IOException ex) {
 			System.err.println(ex);
 		}
+
 	}
 }
--- src/com/sshot/shot.java
+++ src/com/sshot/shot.java
@@ -1,13 +1,16 @@
 package com.sshot;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.swing.JOptionPane;
-
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -21,6 +24,7 @@ public class shot {
 
 	protected Shell shlrdrockcreation;
 	private Text text;
+	private int count=0;
 
 	/**
 	 * Launch the application.
@@ -45,6 +49,9 @@ public class shot {
 		return ft.format(dNow);
 	}
 
+	ArrayList<String> screenshots = new ArrayList<String>();
+	private Text textBeforeScreenShot;
+
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
@@ -55,19 +62,33 @@ public class shot {
 				display.sleep();
 			}
 		}
+		deleteFiles(screenshots);
 	}
 
+	private static void deleteFiles(ArrayList<String> files) {
+		for (String filename : files) {
+			File file = new File(filename);
+			try{
+			if (file.exists()) {
+				file.delete();
+			}}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		}
 	/**
 	 * Create contents of the window.
 	 */
 	protected void createContents() {
+		Map<Integer, String> contents = new HashMap<>();
 		shlrdrockcreation = new Shell();
-		shlrdrockcreation.setSize(324, 78);
-		shlrdrockcreation.setText("3rdRockCreation");
+		shlrdrockcreation.setSize(324, 158);
+		shlrdrockcreation.setText("ScreenShot");
 		text = new Text(shlrdrockcreation, SWT.BORDER);
 
-		text.setBounds(126, 22, 182, 21);
-		ArrayList<String> screenshots = new ArrayList<String>();
+		text.setBounds(137, 91, 161, 21);
 
 		Button stopButton = new Button(shlrdrockcreation, SWT.NONE);
 		stopButton.addSelectionListener(new SelectionAdapter() {
@@ -75,12 +96,13 @@ public class shot {
 			public void widgetSelected(SelectionEvent arg0) {
 				System.out.println(screenshots);
 				try {
+					
 					ShotToWordPad.saveImageToWord(
-							"C:/Users/Public/Pictures/Sample Pictures/" + text.getText() + "__" + getDate() + ".docx",
-							screenshots);
+							"C:/Users/Public/Pictures/" + text.getText() + "__"  + ".docx", screenshots);
 
 					shlrdrockcreation.close();
-					JOptionPane.showMessageDialog(null, "CHECK : C:/Users/Public/Pictures/Sample Pictures/", "Saved", JOptionPane.INFORMATION_MESSAGE);
+					JOptionPane.showMessageDialog(null, "CHECK : C:/Users/Public/Pictures/", "Saved",
+							JOptionPane.INFORMATION_MESSAGE);
 
 				} catch (FileNotFoundException e) {
 
@@ -100,8 +122,16 @@ public class shot {
 		takeScreenShotButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent arg0) {
-				String fileName = "C:/Users/Public/Pictures/testingScreenshots" + getDate() + "-FullScreenshot.jpg";
+				shlrdrockcreation.setVisible(false);
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				String fileName = "C:/Users/Public/Pictures/Take" + getDate() + "-FullScreenshot.jpg";
 				ShotToWordPad.takeScreeShot(fileName);
+				shlrdrockcreation.setVisible(true);
 				screenshots.add(fileName);
 			}
 		});
@@ -112,10 +142,9 @@ public class shot {
 		cancelScreenShotButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent arg0) {
-				try{
+				try {
 					screenshots.remove(screenshots.size() - 1);
-				}
-				catch(ArrayIndexOutOfBoundsException e){
+				} catch (ArrayIndexOutOfBoundsException e) {
 					e.getMessage();
 				}
 			}
@@ -123,8 +152,49 @@ public class shot {
 		cancelScreenShotButton.setText("CANCEL");
 		cancelScreenShotButton.setBounds(208, 0, 100, 21);
 		Label lblDocument = new Label(shlrdrockcreation, SWT.NONE);
-		lblDocument.setBounds(45, 25, 55, 15);
+		lblDocument.setBounds(0, 94, 55, 15);
 		lblDocument.setText("Document :");
+		
+		Label lblText = new Label(shlrdrockcreation, SWT.NONE);
+		lblText.setBounds(0, 27, 125, 15);
+		lblText.setText("Text Before Screenshot :");
+		
+		textBeforeScreenShot = new Text(shlrdrockcreation, SWT.BORDER);
+		textBeforeScreenShot.setBounds(137, 24, 161, 37);
+		
+		
+		Label lblRow = new Label(shlrdrockcreation, SWT.NONE);
+		lblRow.setText("Row :");
+		lblRow.setBounds(174, 67, 33, 15);
+		
+		Button btnNextrow = new Button(shlrdrockcreation, SWT.NONE);
+		btnNextrow.addSelectionListener(new SelectionAdapter() {
 
+			Label label = new Label(shlrdrockcreation, SWT.NONE);
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				count++;
+				label.setText(String.valueOf(count));
+				label.setBounds(216, 67, 33, 15);
+			}
+		});	
+		btnNextrow.setText("NextRow");
+		btnNextrow.setBounds(245, 64, 53, 21);
+		
+		Button btnEnterText = new Button(shlrdrockcreation, SWT.NONE);
+		btnEnterText.setText("Enter Text");
+		btnEnterText.setBounds(60, 40, 63, 21);
+		btnEnterText.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				try {
+					ShotToWordPad.addText("C:/Users/Public/Pictures/" + text.getText() , count, textBeforeScreenShot.getText());
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				contents.put(Integer.valueOf(count), textBeforeScreenShot.getText());
+			}
+		});
 	}
 }
